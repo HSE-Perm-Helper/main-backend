c12af8b1aebf277c232470b6d3eb72356f3ddff4
Denis Malinin <melowetty@mail.ru>
2023-09-16 18:13:27 +0500

    add: equals and hashcode functios to lesson
    * add: priority for all schedule type

--- src/main/kotlin/com/melowetty/hsepermhelper/models/Lesson.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/models/Lesson.kt
@@ -119,4 +119,42 @@ data class Lesson(
     override fun compareTo(other: Lesson): Int {
         return date.compareTo(other.date)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Lesson
+
+        if (subject != other.subject) return false
+        if (course != other.course) return false
+        if (programme != other.programme) return false
+        if (group != other.group) return false
+        if (subGroup != other.subGroup) return false
+        if (date != other.date) return false
+        if (startTimeStr != other.startTimeStr) return false
+        if (endTimeStr != other.endTimeStr) return false
+        if (startTime != other.startTime) return false
+        if (endTime != other.endTime) return false
+        if (lecturer != other.lecturer) return false
+        if (lessonType != other.lessonType) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = subject.hashCode()
+        result = 31 * result + course
+        result = 31 * result + programme.hashCode()
+        result = 31 * result + group.hashCode()
+        result = 31 * result + (subGroup ?: 0)
+        result = 31 * result + date.hashCode()
+        result = 31 * result + startTimeStr.hashCode()
+        result = 31 * result + endTimeStr.hashCode()
+        result = 31 * result + startTime.hashCode()
+        result = 31 * result + endTime.hashCode()
+        result = 31 * result + (lecturer?.hashCode() ?: 0)
+        result = 31 * result + lessonType.hashCode()
+        return result
+    }
 }
