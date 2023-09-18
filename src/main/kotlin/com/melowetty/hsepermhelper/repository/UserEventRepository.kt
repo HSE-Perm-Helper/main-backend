767d58c97c6a286cb7c4c1e530c74848f0723bb9
Denis Malinin <melowetty@mail.ru>
2023-09-18 05:24:58 +0500

    add: user event repository

---
+++ src/main/kotlin/com/melowetty/hsepermhelper/repository/UserEventRepository.kt
@@ -0,0 +1,11 @@
+package com.melowetty.hsepermhelper.repository
+
+import com.melowetty.hsepermhelper.entity.UserEntity
+import com.melowetty.hsepermhelper.entity.UserEventEntity
+import com.melowetty.hsepermhelper.models.UserEventType
+import org.springframework.data.repository.CrudRepository
+
+interface UserEventRepository: CrudRepository<UserEventEntity, Long> {
+    fun findByTargetUser(user: UserEntity)
+    fun findByTargetUserAndUserEventType(user: UserEntity, eventType: UserEventType)
+}
