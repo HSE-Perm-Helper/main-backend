0c7f6ca1fc6788d8a609faf6798fdcadf9fcea23
Denis Malinin <melowetty@mail.ru>
2023-09-19 16:47:43 +0500

    fix: event type

--- src/main/kotlin/com/melowetty/hsepermhelper/events/common/PublicEvent.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/events/common/PublicEvent.kt
@@ -1,16 +1,22 @@
 package com.melowetty.hsepermhelper.events.common
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.melowetty.hsepermhelper.utils.DateUtils
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 open class PublicEvent {
     @JsonProperty("createdTime")
     @Schema(description = "Дата создания ивента")
+    @JsonFormat(pattern = DateUtils.DATE_TIME_PATTERN)
     val date = LocalDateTime.now()
     @JsonProperty("eventType")
     @Schema(description = "Тип ивента")
     fun getEventType(): String {
-        return javaClass.simpleName.split(Regex("(?=\\p{Upper})")).joinToString("_")
+        return javaClass.simpleName
+            .split(Regex("(?=\\p{Upper})"))
+            .filter { it.isNotEmpty() }
+            .joinToString("_") { it.uppercase() }
     }
 }
