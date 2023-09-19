6e762deca87950c39fdb34f7a6ee362c4432dad3
Denis Malinin <melowetty@mail.ru>
2023-09-19 14:20:05 +0500

    remove: unused field

--- src/main/kotlin/com/melowetty/hsepermhelper/events/ScheduleChangedForUserEvent.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/events/ScheduleChangedForUserEvent.kt
@@ -1,12 +1,9 @@
 package com.melowetty.hsepermhelper.events
 
 import com.melowetty.hsepermhelper.events.common.PublicEvent
-import com.melowetty.hsepermhelper.models.ScheduleInfo
 import io.swagger.v3.oas.annotations.media.Schema
 
 class ScheduleChangedForUserEvent(
     @Schema(description = "Список Telegram ID, которым требуется выслать оповещение о изменении в расписании")
     users: List<Long>,
-    @Schema(description = "Расписание, в котором произошло измнение")
-    targetSchedule: ScheduleInfo,
 ): PublicEvent()
