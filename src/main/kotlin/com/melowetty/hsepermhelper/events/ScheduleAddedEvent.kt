ea6406317b6d2eb18a4ab8764f0bb36b1eb01286
Denis Malinin <melowetty@mail.ru>
2023-09-19 13:37:14 +0500

    add: new public events

---
+++ src/main/kotlin/com/melowetty/hsepermhelper/events/ScheduleAddedEvent.kt
@@ -0,0 +1,5 @@
+package com.melowetty.hsepermhelper.events
+
+class ScheduleAddedEvent(
+    id: Long
+): PublicEvent(id)
--- src/main/kotlin/com/melowetty/hsepermhelper/events/ScheduleChangedForUserEvent.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/events/ScheduleChangedForUserEvent.kt
@@ -3,9 +3,10 @@ package com.melowetty.hsepermhelper.events
 import Schedule
 import io.swagger.v3.oas.annotations.media.Schema
 
-data class ScheduleChangedForUserEvent(
+class ScheduleChangedForUserEvent(
+    id: Long,
     @Schema(description = "Список Telegram ID, которым требуется выслать оповещение о изменении в расписании")
-    val users: List<Long>,
+    users: List<Long>,
     @Schema(description = "Расписание, в котором произошло измнение")
-    val targetSchedule: Schedule,
-)
+    targetSchedule: Schedule,
+): PublicEvent(id)
