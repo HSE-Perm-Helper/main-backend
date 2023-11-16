1fd94bf14ef3a2f5a11a100f73ff0fd22ee21584
Denis Malinin <melowetty@mail.ru>
2023-11-16 13:48:17 +0500

    update: build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,7 +63,7 @@ tasks {
         dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
         archiveVersion.set("latest")
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to "com.melowetty.hsepermhelper.HsePermHelper")) } // Provided we set it up in the application plugin configuration
+        manifest { attributes(mapOf("Main-Class" to "com.melowetty.hsepermhelper.HsePermHelperApplication")) } // Provided we set it up in the application plugin configuration
         val sourcesMain = sourceSets.main.get()
         val contents = configurations.runtimeClasspath.get()
             .map { if (it.isDirectory) it else zipTree(it) } +
