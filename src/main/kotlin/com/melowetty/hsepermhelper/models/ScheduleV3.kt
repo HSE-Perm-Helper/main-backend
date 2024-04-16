986eaeec283505fe411c575cbd43923faeef0779
Denis Malinin <melowetty@mail.ru>
2024-04-16 08:13:22 +0500

    change of schema schedule v3

--- src/main/kotlin/com/melowetty/hsepermhelper/models/ScheduleV3.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/models/ScheduleV3.kt
@@ -7,37 +7,25 @@ import java.time.LocalDate
 
 @Schema(description = "Расписание занятий")
 data class ScheduleV3(
-    @Schema(description = "Номер недели", example = "6", nullable = true)
-    val weekNumber: Int?,
+    @Schema(description = "Номер расписания", example = "6", nullable = true)
+    val number: Int?,
     val lessons: List<Lesson>,
     @JsonFormat(pattern = DateUtils.DATE_PATTERN)
-    @Schema(description = "Дата начала недели", example = "03.09.2023", type = "string")
-    val weekStart: LocalDate,
+    @Schema(description = "Дата начала расписания", example = "03.09.2023", type = "string")
+    val start: LocalDate,
     @JsonFormat(pattern = DateUtils.DATE_PATTERN)
-    @Schema(description = "Дата конца недели", example = "10.09.2023", type = "string")
-    val weekEnd: LocalDate,
+    @Schema(description = "Дата конца расписания", example = "10.09.2023", type = "string")
+    val end: LocalDate,
     @Schema(description = "Тип расписания")
     val scheduleType: ScheduleType,
 ) {
     override fun hashCode(): Int {
-        var result = weekNumber ?: 0
+        var result = number ?: 0
         result = 31 * result + lessons.hashCode()
-        result = 31 * result + weekStart.hashCode()
-        result = 31 * result + weekEnd.hashCode()
+        result = 31 * result + start.hashCode()
+        result = 31 * result + end.hashCode()
         result = 31 * result + scheduleType.hashCode()
         return result
-    }
-
-    companion object {
-        fun Schedule.toScheduleInfo(): ScheduleInfo {
-            return ScheduleInfo(
-                weekNumber = weekNumber,
-                weekStart = weekStart,
-                weekEnd = weekEnd,
-                scheduleType = scheduleType,
-                hashcode = hashCode()
-            )
-        }
     }
 }
 
