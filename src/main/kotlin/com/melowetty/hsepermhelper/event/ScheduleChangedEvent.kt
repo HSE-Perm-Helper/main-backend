2c301c0dbe501875a9c319ab599bd33df378d1ce
Denis Malinin <melowetty@mail.ru>
2024-06-24 00:21:39 +0500

    remove unused event

--- src/main/kotlin/com/melowetty/hsepermhelper/event/ScheduleChangedEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.melowetty.hsepermhelper.event
-
-import com.melowetty.hsepermhelper.model.ChangedSchedule
-
-data class ScheduleChangedEvent(
-    val changes: Map<EventType, List<ChangedSchedule>>,
-)
