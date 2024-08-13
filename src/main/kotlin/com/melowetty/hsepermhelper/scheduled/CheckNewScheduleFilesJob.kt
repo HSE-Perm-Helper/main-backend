fec691b77f36ba35aa396e787994171dbcc85eca
Denis Malinin <melowetty@mail.ru>
2024-08-13 12:48:17 +0500

    move scheduled tasks to scheduled package and add logging

---
+++ src/main/kotlin/com/melowetty/hsepermhelper/scheduled/CheckNewScheduleFilesJob.kt
@@ -0,0 +1,32 @@
+package com.melowetty.hsepermhelper.scheduled
+
+import com.melowetty.hsepermhelper.repository.ScheduleFilesRepository
+import com.melowetty.hsepermhelper.service.FilesCheckingChangesService
+import com.melowetty.hsepermhelper.service.ScheduleFilesService
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class CheckNewScheduleFilesJob(
+    private val eventPublisher: ApplicationEventPublisher,
+    private val filesCheckingChangesService: FilesCheckingChangesService,
+    private val repository: ScheduleFilesRepository,
+    private val scheduleFilesService: ScheduleFilesService
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    @Scheduled(fixedRate = 1000 * 60 * 5, initialDelay = 1000 * 60 * 5)
+    fun fetchScheduleFilesAndPublishEvents() {
+        logger.info("Fetching new schedule files")
+        val before = scheduleFilesService.getScheduleFiles()
+        repository.fetchScheduleFiles()
+        val after = scheduleFilesService.getScheduleFiles()
+        val changes = filesCheckingChangesService.getChanges(before = before, after = after)
+        if(changes.addedOrChanged.isNotEmpty() || changes.deleted.isNotEmpty()) {
+            logger.info("Schedule files has changed")
+            eventPublisher.publishEvent(changes)
+        }
+    }
+}
--- src/main/kotlin/com/melowetty/hsepermhelper/service/impl/ScheduleFilesServiceImpl.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/service/impl/ScheduleFilesServiceImpl.kt
@@ -2,29 +2,13 @@ package com.melowetty.hsepermhelper.service.impl
 
 import com.melowetty.hsepermhelper.model.File
 import com.melowetty.hsepermhelper.repository.ScheduleFilesRepository
-import com.melowetty.hsepermhelper.service.FilesCheckingChangesService
 import com.melowetty.hsepermhelper.service.ScheduleFilesService
-import org.springframework.context.ApplicationEventPublisher
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
 class ScheduleFilesServiceImpl(
-    private val eventPublisher: ApplicationEventPublisher,
-    private val filesCheckingChangesService: FilesCheckingChangesService,
     private val repository: ScheduleFilesRepository
 ): ScheduleFilesService {
-    @Scheduled(fixedRate = 1000 * 60 * 5, initialDelay = 1000 * 60 * 5)
-    fun fetchScheduleFilesAndPublishEvents() {
-        val before = getScheduleFiles()
-        repository.fetchScheduleFiles()
-        val after = getScheduleFiles()
-        val changes = filesCheckingChangesService.getChanges(before = before, after = after)
-        if(changes.addedOrChanged.isNotEmpty() || changes.deleted.isNotEmpty()) {
-            eventPublisher.publishEvent(changes)
-        }
-    }
-
     override fun getScheduleFiles(): List<File> {
         return repository.getScheduleFilesAsByteArray().map { File(data = it) }
     }
