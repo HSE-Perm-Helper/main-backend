7c7ba410990d312c38de07ba038e9a1cf84f4896
Denis Malinin <melowetty@mail.ru>
2024-08-16 00:01:13 +0500

    add annotation for logging and change logging in project

--- src/main/kotlin/com/melowetty/hsepermhelper/scheduled/CheckNewScheduleFilesJob.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/scheduled/CheckNewScheduleFilesJob.kt
@@ -1,29 +1,29 @@
 package com.melowetty.hsepermhelper.scheduled
 
+import com.melowetty.hsepermhelper.annotation.Slf4j
+import com.melowetty.hsepermhelper.annotation.Slf4j.Companion.log
 import com.melowetty.hsepermhelper.service.FilesCheckingChangesService
 import com.melowetty.hsepermhelper.service.ScheduleFilesService
-import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
 @Component
+@Slf4j
 class CheckNewScheduleFilesJob(
     private val eventPublisher: ApplicationEventPublisher,
     private val filesCheckingChangesService: FilesCheckingChangesService,
     private val scheduleFilesService: ScheduleFilesService
 ) {
-    private val logger = LoggerFactory.getLogger(javaClass)
-
     @Scheduled(fixedRate = 1000 * 60 * 5, initialDelay = 1000 * 60 * 5)
     fun fetchScheduleFilesAndPublishEvents() {
-        logger.info("Fetching new schedule files")
+        log.debug("Fetching new schedule files")
         val before = scheduleFilesService.getScheduleFiles()
         scheduleFilesService.fetchScheduleFiles()
         val after = scheduleFilesService.getScheduleFiles()
         val changes = filesCheckingChangesService.getChanges(before = before, after = after)
         if(changes.addedOrChanged.isNotEmpty() || changes.deleted.isNotEmpty()) {
-            logger.info("Schedule files has changed")
+            log.debug("Schedule files has changed")
             eventPublisher.publishEvent(changes)
         }
     }
