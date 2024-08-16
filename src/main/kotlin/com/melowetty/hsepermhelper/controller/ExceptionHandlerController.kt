4a014f9d60484a0c0dd4b74ac7203e6fc5a558f2
Denis Malinin <melowetty@mail.ru>
2024-08-17 01:48:04 +0500

    refactoring exception handler and app props

--- src/main/kotlin/com/melowetty/hsepermhelper/controller/ExceptionHandlerController.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/controller/ExceptionHandlerController.kt
@@ -1,10 +1,13 @@
 package com.melowetty.hsepermhelper.controller
 
+import com.melowetty.hsepermhelper.annotation.Slf4j
+import com.melowetty.hsepermhelper.annotation.Slf4j.Companion.log
 import com.melowetty.hsepermhelper.exception.*
 import com.melowetty.hsepermhelper.model.ErrorResponse
 import org.springframework.core.env.Environment
 import org.springframework.core.env.get
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MissingServletRequestParameterException
@@ -15,133 +18,95 @@ import org.springframework.web.servlet.NoHandlerFoundException
 import java.lang.Boolean.parseBoolean
 
 @RestControllerAdvice
+@Slf4j
 class ExceptionHandlerController(
     environment: Environment
 ) {
-    val isDebug = parseBoolean(environment["app.debug.enable"])
+    private val isDebug = parseBoolean(environment["debug"])
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ScheduleNotFoundException::class)
     fun handleScheduleNotFoundException(exception: ScheduleNotFoundException): ResponseEntity<Any> {
-        if (isDebug) {
-            exception.printStackTrace()
-            return exception.toDebugResponseEntity()
-        }
-        return exception.toResponseEntity()
+        return exceptionToDebugOrNormalResponseEntity(exception)
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException::class)
     fun handleRuntimeException(exception: RuntimeException): ResponseEntity<Any> {
-        var response: Any = ErrorResponse(
-            message = exception.message ?: "Произошла ошибка во время выполнения запроса!",
-            code = exception.javaClass.simpleName,
-            status = HttpStatus.INTERNAL_SERVER_ERROR.value())
-        if (isDebug) {
-            exception.printStackTrace()
-            response = (response as ErrorResponse).toDebugResponse(exception)
-        }
-        return ResponseEntity(response, HttpStatus.INTERNAL_SERVER_ERROR)
+        return exceptionToDebugOrNormalResponseEntity(exception,
+            message = "Произошла ошибка во время выполнения запроса!", HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @ExceptionHandler(NoHandlerFoundException::class)
     fun handleNoHandlerFoundException(exception: NoHandlerFoundException): ResponseEntity<Any> {
-        var response: Any = ErrorResponse(
-            message = "Страница не найдена!",
-            code = exception.javaClass.simpleName,
-            status = HttpStatus.NOT_FOUND.value()
-        )
-        if (isDebug) {
-            exception.printStackTrace()
-            response = (response as ErrorResponse).toDebugResponse(exception)
-        }
-        return ResponseEntity(response, HttpStatus.NOT_FOUND)
+        return exceptionToDebugOrNormalResponseEntity(exception,
+            message = "Страница не найдена!", HttpStatus.NOT_FOUND)
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException::class)
     fun handleUserNotFoundException(exception: UserNotFoundException): ResponseEntity<Any> {
-        if (isDebug) {
-            exception.printStackTrace()
-            return exception.toDebugResponseEntity()
-        }
-        return exception.toResponseEntity()
+        return exceptionToDebugOrNormalResponseEntity(exception)
     }
 
     @ExceptionHandler(PermissionDeniedException::class)
     fun handlePermissionDeniedException(exception: PermissionDeniedException): ResponseEntity<Any> {
-        if (isDebug) {
-            exception.printStackTrace()
-            return exception.toDebugResponseEntity()
-        }
-        return exception.toResponseEntity()
+        return exceptionToDebugOrNormalResponseEntity(exception)
     }
 
     @ExceptionHandler(UnauthorizedException::class)
     fun handleUnauthorizedException(exception: UnauthorizedException): ResponseEntity<Any> {
-        if (isDebug) {
-            exception.printStackTrace()
-            return exception.toDebugResponseEntity()
-        }
-        return exception.toResponseEntity()
+        return exceptionToDebugOrNormalResponseEntity(exception)
     }
 
     @ExceptionHandler(SecretKeyParseException::class)
     fun handleSecretKeyParseException(exception: SecretKeyParseException): ResponseEntity<Any> {
-        if (isDebug) {
-            exception.printStackTrace()
-            return exception.toDebugResponseEntity()
-        }
-        return exception.toResponseEntity()
+        return exceptionToDebugOrNormalResponseEntity(exception)
     }
 
     @ExceptionHandler(MissingServletRequestParameterException::class)
     fun handleMissingServletRequestParameterException(exception: MissingServletRequestParameterException): ResponseEntity<Any> {
-        var response: Any = ErrorResponse(
+        return exceptionToDebugOrNormalResponseEntity(exception,
             message = "Необходимый параметр запроса ${exception.parameterName} типа ${exception.parameterType} не передан!",
-            code = exception.javaClass.simpleName,
-            status = HttpStatus.BAD_REQUEST.value()
-        )
-        if (isDebug) {
-            exception.printStackTrace()
-            response = (response as ErrorResponse).toDebugResponse(exception)
-        }
-        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+            statusCode = HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(HttpMessageNotReadableException::class)
     fun handleHttpMessageNotReadableException(exception: HttpMessageNotReadableException): ResponseEntity<Any> {
-        var response: Any = ErrorResponse(
-            message = "Необходимое тело запроса не передано!",
-            code = exception.javaClass.simpleName,
-            status = HttpStatus.BAD_REQUEST.value()
-        )
-        if (isDebug) {
-            exception.printStackTrace()
-            response = (response as ErrorResponse).toDebugResponse(exception)
-        }
-        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+        return exceptionToDebugOrNormalResponseEntity(exception,
+            message = "Необходимое тело запроса не передано!", HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgumentException(exception: IllegalArgumentException): ResponseEntity<Any> {
-        var response: Any = ErrorResponse(
-            message = exception.message ?: "Неверный параметр в запросе!",
-            code = exception.javaClass.simpleName,
-            status = HttpStatus.BAD_REQUEST.value()
-        )
-        if (isDebug) {
-            exception.printStackTrace()
-            response = (response as ErrorResponse).toDebugResponse(exception)
-        }
-        return ResponseEntity(response, HttpStatus.BAD_REQUEST)
+       return exceptionToDebugOrNormalResponseEntity(exception,
+           "Неверный параметр в запросе!", HttpStatus.BAD_REQUEST)
     }
 
     @ExceptionHandler(UserIsExistsException::class)
     fun handleUserIsExistsException(exception: UserIsExistsException): ResponseEntity<Any> {
+        return exceptionToDebugOrNormalResponseEntity(exception)
+    }
+
+    private fun exceptionToDebugOrNormalResponseEntity(exception: CustomException): ResponseEntity<Any> {
         if (isDebug) {
-            exception.printStackTrace()
+            log.error(exception.stackTraceToString())
             return exception.toDebugResponseEntity()
         }
         return exception.toResponseEntity()
+    }
+
+    private fun exceptionToDebugOrNormalResponseEntity(exception: Exception, message: String, statusCode: HttpStatusCode): ResponseEntity<Any> {
+        val response = ErrorResponse(
+            message = exception.message ?: message,
+            code = exception.javaClass.simpleName,
+            status = statusCode.value(),
+        )
+
+        if (isDebug) {
+            log.error(exception.stackTraceToString())
+            return ResponseEntity(response.toDebugResponse(exception), statusCode)
+        }
+        return ResponseEntity(response, statusCode)
     }
 }
