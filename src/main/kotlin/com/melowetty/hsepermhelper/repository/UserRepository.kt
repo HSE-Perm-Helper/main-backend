3db07bf133e4f2bd6855c7b5bb6733f7e208e79d
Denis Malinin <melowetty@mail.ru>
2024-09-18 23:31:01 +0500

    add coming lessons notification

--- src/main/kotlin/com/melowetty/hsepermhelper/repository/UserRepository.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/repository/UserRepository.kt
@@ -8,7 +8,7 @@ import java.util.UUID
 
 @Repository
 interface UserRepository : CrudRepository<UserEntity, UUID> {
-    fun findAllBySettings_EnabledComingLessonsNotifications(enabledComingNotifications: Boolean): List<UserEntity>
+    fun findAllBySettings_IsEnabledComingLessonsNotifications(enabledComingNotifications: Boolean): List<UserEntity>
     fun findByTelegramId(telegramId: Long): Optional<UserEntity>
     fun existsByTelegramId(telegramId: Long): Boolean
     fun findAllBySettingsGroupAndSettingsSubGroup(group: String, subGroup: Int): List<UserEntity>
