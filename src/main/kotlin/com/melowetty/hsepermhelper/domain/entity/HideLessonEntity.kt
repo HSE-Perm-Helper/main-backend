4bc1d69399455f4b1f6bef35e65a43ae752dae34
Denis Malinin <melowetty@mail.ru>
2024-10-15 10:46:39 +0500

    change name of table for hide lesson entity

--- src/main/kotlin/com/melowetty/hsepermhelper/domain/entity/HideLessonEntity.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/domain/entity/HideLessonEntity.kt
@@ -9,7 +9,7 @@ import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
-@Table(name = "hide_subject")
+@Table(name = "hide_lesson")
 data class HideLessonEntity(
     @Id
     val id: Long,
