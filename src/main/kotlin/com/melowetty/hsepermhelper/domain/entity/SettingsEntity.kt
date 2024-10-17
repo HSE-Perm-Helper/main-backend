d2fb0af28f55f306fafb47113fa2124e00e30833
Denis Malinin <melowetty@mail.ru>
2024-10-17 05:24:14 +0500

    fix fetch hidden lessons entities

--- src/main/kotlin/com/melowetty/hsepermhelper/domain/entity/SettingsEntity.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/domain/entity/SettingsEntity.kt
@@ -3,6 +3,7 @@ package com.melowetty.hsepermhelper.domain.entity
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -23,7 +24,7 @@ data class SettingsEntity(
     @Column(name = "user_sub_group")
     val subGroup: Int = 0,
 
-    @OneToMany(orphanRemoval = true, cascade = [CascadeType.ALL])
+    @OneToMany(orphanRemoval = true, cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
     val hiddenLessons: Set<HideLessonEntity>,
 
     @Column(name = "is_enabled_new_schedule_notification", columnDefinition = "BOOLEAN DEFAULT true")
