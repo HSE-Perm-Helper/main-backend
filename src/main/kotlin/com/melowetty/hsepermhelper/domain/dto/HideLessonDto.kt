149475d9d66edd899ce86424b5329d1d46767d2a
Denis Malinin <melowetty@mail.ru>
2024-10-18 14:23:30 +0500

    fix hidden lessons clearing when settings is changed

--- src/main/kotlin/com/melowetty/hsepermhelper/domain/dto/HideLessonDto.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/domain/dto/HideLessonDto.kt
@@ -1,8 +1,12 @@
 package com.melowetty.hsepermhelper.domain.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.melowetty.hsepermhelper.model.LessonType
 
 data class HideLessonDto(
+    @JsonIgnore
+    val id: Long,
+
     val lesson: String,
 
     val lessonType: LessonType,
--- src/main/kotlin/com/melowetty/hsepermhelper/extension/HideLessonExtension.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/extension/HideLessonExtension.kt
@@ -7,6 +7,16 @@ class HideLessonExtension {
     companion object {
         fun HideLessonEntity.toDto(): HideLessonDto {
             return HideLessonDto(
+                id = id ?: 0,
+                lesson = lesson,
+                lessonType = lessonType,
+                subGroup = subGroup,
+            )
+        }
+
+        fun HideLessonDto.toEntity(): HideLessonEntity {
+            return HideLessonEntity(
+                id = id,
                 lesson = lesson,
                 lessonType = lessonType,
                 subGroup = subGroup,
