00f8c17f3cf3299d018ef78a0c3f868ef4384361
Denis Malinin <melowetty@mail.ru>
2025-01-10 16:41:32 +0500

    fix feign

--- src/main/kotlin/com/melowetty/hsepermhelper/HsePermHelperApplication.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/HsePermHelperApplication.kt
@@ -3,6 +3,7 @@ package com.melowetty.hsepermhelper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 @EnableScheduling
 @EnableDiscoveryClient
 @EnableKafka
+@EnableFeignClients
 class HsePermHelperApplication
 
 fun main(args: Array<String>) {
