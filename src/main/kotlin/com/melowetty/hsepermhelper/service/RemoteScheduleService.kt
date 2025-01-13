3097677f11ae7f7fb377d9acce1cf5983d38331d
Denis Malinin <melowetty@mail.ru>
2025-01-14 02:23:05 +0500

    fix remote schedule

--- src/main/kotlin/com/melowetty/hsepermhelper/service/RemoteScheduleService.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/service/RemoteScheduleService.kt
@@ -1,18 +1,17 @@
 package com.melowetty.hsepermhelper.service
 
-import com.melowetty.hsepermhelper.fallback.RemoteScheduleServiceFallback
 import com.melowetty.hsepermhelper.model.RemoteScheduleToken
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 
-@FeignClient("remote-schedule-service", url = "\${api.remote-schedule-service.url:}", fallbackFactory = RemoteScheduleServiceFallback::class)
+@FeignClient("remote-schedule-service", url = "\${api.remote-schedule-service.url:}")
 interface RemoteScheduleService {
     @GetMapping("/remote-schedule-management")
     fun getUserScheduleToken(
         @RequestParam("telegramId") telegramId: Long,
-    ): RemoteScheduleToken?
+    ): RemoteScheduleToken
 
     @PostMapping("/remote-schedule-management")
     fun createOrUpdateUserScheduleToken(
