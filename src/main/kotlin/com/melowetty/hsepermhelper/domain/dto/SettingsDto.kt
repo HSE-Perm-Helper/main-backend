635502da9f067934ace55d61b76e9cc846ced41a
Denis Malinin <melowetty@mail.ru>
2025-02-05 20:36:14 +0500

    fix: mapping settings dto

--- src/main/kotlin/com/melowetty/hsepermhelper/domain/dto/SettingsDto.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/domain/dto/SettingsDto.kt
@@ -8,7 +8,7 @@ data class SettingsDto(
     @JsonIgnore
     val id: Long? = null,
 
-    val email: String?,
+    val email: String? = null,
 
     @Schema(description = "Учебная группа пользователя", example = "РИС-22-3")
     val group: String = "",
