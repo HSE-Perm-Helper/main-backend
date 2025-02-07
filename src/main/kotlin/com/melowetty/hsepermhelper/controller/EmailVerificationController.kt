4c44a2cbeb4e27a733bb33649db13139c0e0e65e
Denis Malinin <melowetty@mail.ru>
2025-02-07 23:48:32 +0500

    add: email verification controller

---
+++ src/main/kotlin/com/melowetty/hsepermhelper/controller/EmailVerificationController.kt
@@ -0,0 +1,32 @@
+package com.melowetty.hsepermhelper.controller
+
+import com.melowetty.hsepermhelper.domain.dto.EmailVerificationDto
+import com.melowetty.hsepermhelper.service.EmailVerificationService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("verification")
+class EmailVerificationController(
+    private val emailVerificationService: EmailVerificationService
+) {
+    @GetMapping("{secret}")
+    fun verifyEmailBySecret(@PathVariable("secret") secret: String): String {
+        emailVerificationService.checkVerificationSecret(secret)
+        return "Почта успешно подтверждена, эту страницу можно закрыть"
+    }
+
+    @PostMapping("{token}")
+    fun resendVerificationMessage(@PathVariable("token") token: String): EmailVerificationDto {
+        return emailVerificationService.resendVerificationLink(token)
+    }
+
+    @DeleteMapping("{token}")
+    fun cancelVerification(@PathVariable("token") token: String) {
+        return emailVerificationService.cancelVerification(token)
+    }
+}
