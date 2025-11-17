1276ea46e373fa6658965db935622313f3857d10
Denis Malinin <melowetty@mail.ru>
2025-11-18 02:03:50 +0500

    fix logging context for new threads

--- src/main/kotlin/com/melowetty/hsepermhelper/config/MultithreadingConfig.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/config/MultithreadingConfig.kt
@@ -8,21 +8,41 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class MultithreadingConfig {
+
     @Bean("check-changes-from-hse-api-executor-service")
     fun executorServiceForHseApiCheckingChanges(): ExecutorService {
-        return Executors.newFixedThreadPool(10)
+        return Executors.newFixedThreadPool(10) { runnable ->
+            newThreadWithMdc(runnable, "hse-api-checker")
+        }
     }
 
     @Bean("add-user-events-executor-service")
     fun executorServiceForAddingUserEvents(): ExecutorService {
         return Executors.newFixedThreadPool(10) { runnable ->
-            Thread {
-                val context = MDC.getCopyOfContextMap()
+            newThreadWithMdc(runnable, "user-events")
+        }
+    }
+
+    private fun newThreadWithMdc(runnable: Runnable, name: String): Thread {
+        val contextMap = MDC.getCopyOfContextMap()
+
+        val thread = Thread(runnable, name)
+        thread.setUncaughtExceptionHandler { _, e ->
+            e.printStackTrace()
+        }
+        return object : Thread(runnable, name) {
+            override fun run() {
+                val previous = MDC.getCopyOfContextMap()
                 try {
-                    if (context != null) MDC.setContextMap(context)
-                    runnable.run()
+                    if (contextMap != null) {
+                        MDC.setContextMap(contextMap)
+                    }
+                    super.run()
                 } finally {
                     MDC.clear()
+                    if (previous != null) {
+                        MDC.setContextMap(previous)
+                    }
                 }
             }
         }
