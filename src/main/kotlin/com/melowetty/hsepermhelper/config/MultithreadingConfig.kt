1140c97ce8a51d2204e83f925505c0fcb74ce005
Denis Malinin <melowetty@mail.ru>
2025-11-18 02:03:50 +0500

    fix logging context for new threads

--- src/main/kotlin/com/melowetty/hsepermhelper/config/MultithreadingConfig.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/config/MultithreadingConfig.kt
@@ -1,13 +1,16 @@
 package com.melowetty.hsepermhelper.config
 
 import org.slf4j.MDC
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.task.TaskDecorator
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 @Configuration
 class MultithreadingConfig {
+
     @Bean("check-changes-from-hse-api-executor-service")
     fun executorServiceForHseApiCheckingChanges(): ExecutorService {
         return Executors.newFixedThreadPool(10)
@@ -15,14 +18,21 @@ class MultithreadingConfig {
 
     @Bean("add-user-events-executor-service")
     fun executorServiceForAddingUserEvents(): ExecutorService {
-        return Executors.newFixedThreadPool(10) { runnable ->
-            Thread {
-                val context = MDC.getCopyOfContextMap()
+        val executor = ThreadPoolTaskExecutor();
+        executor.setTaskDecorator(MDCTaskDecorator());
+        executor.initialize();
+        return executor.threadPoolExecutor
+    }
+
+    class MDCTaskDecorator : TaskDecorator {
+        override fun decorate(runnable: Runnable): Runnable {
+            val contextMap = MDC.getCopyOfContextMap();
+            return Runnable {
                 try {
-                    if (context != null) MDC.setContextMap(context)
-                    runnable.run()
+                    MDC.setContextMap(contextMap);
+                    runnable.run();
                 } finally {
-                    MDC.clear()
+                    MDC.clear();
                 }
             }
         }
