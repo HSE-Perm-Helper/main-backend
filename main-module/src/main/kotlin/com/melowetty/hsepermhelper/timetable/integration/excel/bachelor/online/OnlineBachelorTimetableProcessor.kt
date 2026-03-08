72afcbccf08db678d58aafa7bd231893caabda65
Bogdan <bogdan@MacBook-Air-Bogdan.local>
2026-03-08 18:01:38 +0100

    split online bach table per week

--- main-module/src/main/kotlin/com/melowetty/hsepermhelper/timetable/integration/excel/bachelor/online/OnlineBachelorTimetableProcessor.kt
+++ main-module/src/main/kotlin/com/melowetty/hsepermhelper/timetable/integration/excel/bachelor/online/OnlineBachelorTimetableProcessor.kt
@@ -2,7 +2,6 @@ package com.melowetty.hsepermhelper.timetable.integration.excel.bachelor.online
 
 import com.melowetty.hsepermhelper.timetable.integration.excel.ExcelTimetableProcessor
 import com.melowetty.hsepermhelper.timetable.integration.excel.bachelor.shared.TimetableInfoUtils
-import com.melowetty.hsepermhelper.timetable.integration.excel.bachelor.shared.TimetableParseUtils
 import com.melowetty.hsepermhelper.domain.model.timetable.EducationType
 import com.melowetty.hsepermhelper.domain.model.timetable.ExcelTimetable
 import com.melowetty.hsepermhelper.domain.model.timetable.InternalTimetableSource
@@ -12,30 +11,31 @@ import org.springframework.stereotype.Component
 @Component
 class OnlineBachelorTimetableProcessor : ExcelTimetableProcessor {
     override fun process(data: Workbook): List<ExcelTimetable> {
-        val scheduleInfo = TimetableInfoUtils.getTimetableInfoIteratively(data)
-            ?: throw IllegalArgumentException("Can't find timetable info")
+        val timetables = mutableListOf<ExcelTimetable>()
 
-        val lessons = TimetableParseUtils.parseSheets(
-            data,
-            sheetParser = { sheet ->
-                OnlineTimetableLessonsUtils.parseSheet(sheet, scheduleInfo) { cellInfo ->
-                    OnlineTimetableCellParser.parseLesson(cellInfo)
-                }
+        for (i in 0 until data.numberOfSheets) {
+            val sheet = data.getSheetAt(i)
+            val scheduleInfo = TimetableInfoUtils.getScheduleInfoBySheet(sheet) ?: continue
+
+            val lessons = OnlineTimetableLessonsUtils.parseSheet(sheet, scheduleInfo) { cellInfo ->
+                OnlineTimetableCellParser.parseLesson(cellInfo)
             }
-        )
 
-        return listOf(
-            ExcelTimetable(
-                number = scheduleInfo.number,
-                lessons = lessons,
-                start = scheduleInfo.startDate,
-                end = scheduleInfo.endDate,
-                type = scheduleInfo.type,
-                educationType = EducationType.BACHELOR_ONLINE,
-                isParent = true,
-                source = InternalTimetableSource.EXCEL,
+            timetables.add(
+                ExcelTimetable(
+                    number = scheduleInfo.number,
+                    lessons = lessons,
+                    start = scheduleInfo.startDate,
+                    end = scheduleInfo.endDate,
+                    type = scheduleInfo.type,
+                    educationType = EducationType.BACHELOR_ONLINE,
+                    isParent = true,
+                    source = InternalTimetableSource.EXCEL,
+                )
             )
-        )
+        }
+
+        return timetables
     }
 
     override fun isParseable(name: String): Boolean {
